15a9fce04f715f2aa251369cf2f2c36919201e95
happyhumorist <happyhumorist>
2026-08-31 15:48:07 -0500

    made Samurai scripts better

--- include/constants/flags_frlg.h
+++ include/constants/flags_frlg.h
@@ -155,7 +155,7 @@
 #define FLAG_HIDE_VIRIDIAN_FOREST_GBALL                 0x0B2
 #define FLAG_HIDE_VIRIDIAN_FOREST_NEESHA                0x0B3
 #define FLAG_GOT_CHARMANDER                             0x0B4
-#define FLAG_0x0B5               0x0B5
+#define FLAG_HIDE_VIRIDIAN_FOREST_SAMURAI               0x0B5
 #define FLAG_0x0B6               0x0B6
 #define FLAG_0x0B7               0x0B7
 #define FLAG_0x0B8               0x0B8
